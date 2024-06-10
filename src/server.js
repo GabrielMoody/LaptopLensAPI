a46dbf9adb6f14138fe340a0109e6dc7b79a6af8
root <root@LAPTOP-BE4IDVF0.localdomain>
2024-06-10 15:14:19 +0700

    edit API

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
-const hapi = require('@hapi/hapi')
+const express = require("express");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
 
-const init = async() => {
-  const server = hapi.server({
-    port: 3000,
-    host: "localhost"
-  })
 
-  await server.start()
-  console.log(`Server running on ${server.info.uri}`)
-}
+const PORT = 3000;
+const app = express();
 
-init()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
